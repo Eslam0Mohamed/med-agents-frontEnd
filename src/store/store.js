213d3136cdc6da4e991dfe8dd712d96924d76035
Eslam Mohamed <eslama202002@gmail.com>
2026-06-22 11:44:45 +0300

    complete patient module

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'; 
+import patientsReducer from "../slices/patientsSlice"
 export const store = configureStore({
   reducer: {
-    
+    patients: patientsReducer
   },
 });
  
