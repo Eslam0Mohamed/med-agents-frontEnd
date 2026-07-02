00363bf3fdbf1f5616ff5a91f7e68f0107f08495
ShazaElsayedIsmail11 <elsayedshaza9@gmail.com>
2026-07-02 16:06:20 +0300

    follow up update

--- src/pages/consultations/ConsultationForm.jsx
+++ src/pages/consultations/ConsultationForm.jsx
@@ -125,7 +125,7 @@ useEffect(() => {
     
     setPatientSearch(patient.name);
     setSelectedPatientId(patient._id);
-    setValue('patientId', patient._id, { shouldValidate: true, shouldDirty: true });
+    setValue('patientId', patient._id, { dValidate: true, shouldDirty: true });
     setShowDropdown(false);
   };
 
