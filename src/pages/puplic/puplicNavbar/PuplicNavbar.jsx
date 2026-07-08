ade999dc77f144a7492579ad1479f20a79464aff
saraashour040-tech <saraashour040@gmail.com>
2026-07-08 08:17:00 +0300

    update  index html  update website favicon  and remove stethoscope icon from navbar logo

--- src/pages/puplic/puplicNavbar/PuplicNavbar.jsx
+++ src/pages/puplic/puplicNavbar/PuplicNavbar.jsx
@@ -30,12 +30,7 @@ const PublicNavbar = ({ isDark, toggleTheme }) => {
             <svg className="w-5 h-5 text-blue-600 animate-pulse" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
             </svg>
-            <svg className="w-5 h-5 text-blue-500" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M4.8 8H12a5 5 0 0 1 5 5v3a3 3 0 0 1-3 3H8a3 3 0 0 1-3-3v-3a5 5 0 0 1 5-5h2.2" />
-              <path d="M5 8V4h4" />
-              <path d="M19 8V4h-4" />
-              <circle cx="12" cy="18" r="2" />
-            </svg>
+           
           </div>
           <span>Med<span className="text-blue-600">Agents</span></span>
         </Link>
